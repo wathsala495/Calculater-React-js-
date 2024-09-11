42f1a6ebb5da5749684ebe388d61a17cb19fe589
wathsala495 <sumuduwathsala80@gmail.com>
2024-09-11 12:38:05 +0530

    calculater

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
+export default function App() {
+  const [value,setValue]=useState('');
+  
+  function deleteNUm(){
+    setValue('')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   <div className="container flex justify-center items-center h-screen">
+     <div className="calculator">
+      <form action="">
+        <div className="">
+           <input type="text" value={value} className="border w-[340px] h-[60px] bg-indigo-500 rounded-lg p-4 text-end text-2xl font-bold"/>
+           </div>
+        <div> 
+          <input type="text" value="AC" onClick={(e)=> setValue('')} className=" caret-transparent border w-[80px] p-2  text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold  cursor-pointer"/>
+          <input type="text" value="DE" onClick={(e)=>setValue(value.slice(0, -1))} className="caret-transparent border w-[80px] p-2 text-center  mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer "/>
+          <input type="text" value="." onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center  mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="/" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center  mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+        </div>
+        <div> 
+        <input type="text" value="7" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2  text-center  mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="8" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center  mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="9" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="*" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+        </div>
+        <div> 
+        <input type="text" value="4" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="5" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="6" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2  rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="+" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+        </div>
+        <div>
+        <input type="text" value="1" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl  font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="2" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="3" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="-" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/> </div>
+        <div> 
+        <input type="text" value="00" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="0" onClick={(e)=>setValue(value+e.target.value)} className="border w-[80px] p-2 text-center mt-2 mr-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+          <input type="text" value="=" onClick={(e)=>setValue(eval(value))} className="border w-[168px] p-2  text-center mt-2 rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-2xl font-bold cursor-pointer caret-transparent"/>
+          
+        </div>
 
-export default App;
+      </form>
+     </div>
+   </div>
+  )
+}
